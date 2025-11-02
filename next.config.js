7bd53fa90f06ae3a365f82c9fd0864e482dc49c1
Faisal <faisalslh97@gmail.com>
2025-11-02 18:38:21 +0700

    hapus docker

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
+  // output: "standalone",  <-- hapus atau komentar baris ini
 };
 
 module.exports = nextConfig;
