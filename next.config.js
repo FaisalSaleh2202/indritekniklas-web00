5a23f89b6fb4aebf94ee4b2559b098ae73753faf
Faisal <faisalslh97@gmail.com>
2025-11-13 15:44:37 +0700

    version3

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // output: "standalone",  <-- hapus atau komentar baris ini
+  reactStrictMode: false,
+  experimental: {
+    turbo: false, // kadang turbopack bug dengan radix id
+  },
 };
 
 module.exports = nextConfig;
